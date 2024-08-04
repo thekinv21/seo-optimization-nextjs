f5097b77dc4564fcf9cf07dc355f72f05fa1c30e
Vadim Kiniabaev <thekinv21@gmail.com>
2024-08-04 17:15:24 +0300

    FEAT:[SEO][Vadim]: microPattern meta is added

---
+++ src/app/(public)/page.item.ts
@@ -0,0 +1,28 @@
+export type TypeItem = {
+	url: string
+	title: string
+	description: string
+}
+
+export const items: TypeItem[] = [
+	{
+		url: '#',
+		title: 'Docs',
+		description: 'Find in-depth information about Next.js features and API.',
+	},
+	{
+		url: '#',
+		title: 'Learn',
+		description: 'Learn about Next.js in an interactive course with quizzes!',
+	},
+	{
+		url: '#',
+		title: 'Templates',
+		description: 'Explore starter templates for Next.js.',
+	},
+	{
+		url: '#',
+		title: 'Deploy',
+		description: 'Instantly deploy your Next.js site to a public URL',
+	},
+]
